ce42581c955a99db77879cb9f6107ce7d56daa8d
yaohong <yhchun1992@hotmail.com>
2015-07-16 23:04:03 +1200

    gallery and about page filled with placeholders

--- app.js
+++ app.js
@@ -6,9 +6,37 @@ abcApp.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider
             .when('/about', {
-                templateUrl: 'views/partials/about.html'
+                templateUrl: 'views/partials/about.html',
+                controller: 'aboutController'
             })
             .otherwise({
-                templateUrl: 'views/partials/home.html'
+                templateUrl: 'views/partials/gallery.html',
+                controller: 'galleryController'
             });
     }]);
+
+abcApp.controller('mainController', function ($scope) {
+
+});
+
+
+abcApp.controller('galleryController', function ($scope) {
+    $scope.pageTitle = "Gallery";
+
+    $scope.$on('$viewContentLoaded', function () {
+        $(document).ready(function () {
+            $(".owl-carousel").owlCarousel({
+                items: 2,
+                lazyLoad: true,
+                loop: true,
+                margin: 10,
+                itemsScaleUp: true
+            });
+        });
+    });
+
+});
+
+abcApp.controller('aboutController', function ($scope) {
+    $scope.pageTitle = "About";
+});
